6a25440753906355023bc025c648726669f60f82
Daniel Quack <daniel.q@posteo.de>
2015-10-28 21:39:57 +0100

    added changes by Nehal
    - if else construct
    - delete comments before get and set methods

--- src/main/java/de/unistuttgart/dsaws2015/ex0/p1/Manufacturer.java
+++ src/main/java/de/unistuttgart/dsaws2015/ex0/p1/Manufacturer.java
@@ -14,58 +14,42 @@ public class Manufacturer {
 		this.country="DefaultCountry";
 	}
 	
-	/**
-	 * @return the name
-	 */
+
 	public String getName() {
 		return name;
 	}
 
-	/**
-	 * @param name the name to set
-	 */
+
 	public void setName(String name) {
 		this.name = name;
 	}
 
-	/**
-	 * @return the street
-	 */
+
 	public String getStreet() {
 		return street;
 	}
 
-	/**
-	 * @param street the street to set
-	 */
+
 	public void setStreet(String street) {
 		this.street = street;
 	}
 
-	/**
-	 * @return the city
-	 */
+
 	public String getCity() {
 		return city;
 	}
 
-	/**
-	 * @param city the city to set
-	 */
+
 	public void setCity(String city) {
 		this.city = city;
 	}
 
-	/**
-	 * @return the country
-	 */
+
 	public String getCountry() {
 		return country;
 	}
 
-	/**
-	 * @param country the country to set
-	 */
+
 	public void setCountry(String country) {
 		this.country = country;
 	}
--- src/main/java/de/unistuttgart/dsaws2015/ex0/p1/TextFinder.java
+++ src/main/java/de/unistuttgart/dsaws2015/ex0/p1/TextFinder.java
@@ -5,50 +5,45 @@ public class TextFinder {
 	/**
 	 * This methods search for a specific string in the given class Product and it's 
 	 * corresponding class Manufacturer.
-	 * If the string is found, the methods returns the corresponding {@link ProductFact} enum.
+	 * If the string is found, the methods returns the corresponding {@link ProductFact} enumeration.
 	 * If the string is not present in the given classes or an invalid input parameter is given, 
 	 * the function throws an {@link IllegalArgumentException}.
 	 * 
 	 * @param product {@link Product} which will be searched for the string.
-	 * @param string {@link String} which is searched in the product.
-	 * @return {@link ProductFact} The enum which indicates the type of the string.
-	 * @throws IllegalArgumentException if the input values are not correct or the string is not
+	 * @param string {@link String} which is searched for in the attributes of the product class and its corresponding manufacturer class.
+	 * @return {@link ProductFact} The enumeration which indicates the type of the string.
+	 * @throws IllegalArgumentException if the arguments are invalid or the string is not
 	 * present in the given classes.
 	 */
 	public static ProductFact findText(Product product, String string) { 
 
 		// check for invalid input parameters (null)
-		if (string==null) {
-			throw new IllegalArgumentException("null String not allowed!");
-		}
-		if (product==null) {
-			throw new IllegalArgumentException("null Prodcut not allowed!");
-		}
+		if (string==null||product==null)
+			throw new IllegalArgumentException("Invalid product or string argument");
 		if (product.getManufacturer()==null) {
 			throw new IllegalArgumentException("Product is not connected to a Manufacturer");
 		}
 		
 		// check the strings of the class Products
-		if (product.getName() == string) {
+		if (product.getName() == string)
 			return ProductFact.NAME;
-		}
-		if (product.getDescription() == string) {
-			return ProductFact.DESCRIPTION;	
-		}
-		
-		// check the strings of the class Manufacturer
-		if (product.getManufacturer().getName() == string) {
-			return ProductFact.MANUFACTURER_NAME;	
-		}
-		if (product.getManufacturer().getStreet() == string) {
-			return ProductFact.MANFACTURER_STREET;	
-		}
-		if(product.getManufacturer().getCity() == string) {
-			return ProductFact.MANUFACTURER_CITY;	
-		}
-		if(product.getManufacturer().getCountry() == string) {
-			return ProductFact.MANUFACTURER_COUNTRY;	
-		}
-		throw new IllegalArgumentException("String not found!");
+		else
+			if (product.getDescription() == string)
+				return ProductFact.DESCRIPTION;	
+		    // check the strings of the class Manufacturer
+			else
+				if (product.getManufacturer().getName() == string)
+						return ProductFact.MANUFACTURER_NAME;	
+				else
+					if (product.getManufacturer().getStreet() == string)
+						return ProductFact.MANFACTURER_STREET;	
+					else
+						if(product.getManufacturer().getCity() == string)
+							return ProductFact.MANUFACTURER_CITY;	
+						else
+							if(product.getManufacturer().getCountry() == string)
+								return ProductFact.MANUFACTURER_COUNTRY;	
+							else
+								throw new IllegalArgumentException("String not found!"); // no string was found in Product and Manufacturer:
+			}
 	}
-}
